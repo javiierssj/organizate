b1ed3d3d4cdb0ff0f59b307892b36922f2d04ae4
javiierssj <jav.guzmanp@duocuc.cl>
2024-01-03 19:55:43 -0300

    inicio

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'organizate',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
